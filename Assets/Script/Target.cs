9ea347f0f8bc493a1d30af9688443ba93d96de6f
Prometheus <prometheus@polispay.org>
2019-04-04 20:51:34 -0600

    Some changes + enemy follow

---
+++ Assets/Script/Target.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float health = 50f;
+    public Animator animator;
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        Debug.Log(health.ToString());
+        if(health <= 0f)
+        {
+            StartCoroutine(KillEnemy());
+        }
+    }
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    IEnumerator KillEnemy()
+    {
+        if (animator != null)
+            animator.SetBool("isDead", true);
+
+        yield return new WaitForSeconds(3.0f);
+        Die();
+    }
+}
---
+++ Assets/Script/enemy.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemy : MonoBehaviour
+{
+    NavMeshAgent nm;
+    public Transform target;
+    public enum ZombieState { idle, chasing };
+    public float zombieArea = 10f;
+    public Animator animator; 
+
+    public ZombieState state = ZombieState.idle;
+
+    void Start()
+    {
+        nm = GetComponent<NavMeshAgent>();
+        StartCoroutine(UseBrain());
+    }
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator UseBrain()
+    {
+        while (true)
+        {
+            switch (state)
+            {
+                case ZombieState.idle:
+                    float dist = Vector3.Distance(target.position, transform.position);
+                    if (dist < zombieArea && animator != null)
+                    {
+                        state = ZombieState.chasing;
+                        animator.SetBool("isChasing", true);
+                    }
+                    nm.SetDestination(transform.position);
+                    break;
+                case ZombieState.chasing:
+                    dist = Vector3.Distance(target.position, transform.position);
+                    if (dist > zombieArea && animator != null)
+                    {
+                        state = ZombieState.idle;
+                        animator.SetBool("isChasing", false);
+                    }
+                    nm.SetDestination(target.position);
+                    break;
+                default:
+                    break;
+            }
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+}
